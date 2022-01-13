b0d884950a600ce12d10f9bee2027d318b11e6c4
Alice Frosi <afrosi@redhat.com>
2022-01-13 13:17:05 +0100

    Add func test for multiple device with the same resource name

--- tests/vmi_hostdev_test.go
+++ tests/vmi_hostdev_test.go
@@ -1,15 +1,18 @@
 package tests_test
 
 import (
+	"fmt"
 	"strings"
 
 	expect "github.com/google/goexpect"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	"kubevirt.io/kubevirt/tests/util"
 
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
@@ -17,6 +20,10 @@ import (
 	virtconfig "kubevirt.io/kubevirt/pkg/virt-config"
 	"kubevirt.io/kubevirt/tests"
 	"kubevirt.io/kubevirt/tests/console"
+)
+
+const (
+	failedDeleteVMI = "Failed to delete VMI"
 )
 
 var _ = Describe("[Serial][sig-compute]HostDevices", func() {
@@ -29,9 +36,8 @@ var _ = Describe("[Serial][sig-compute]HostDevices", func() {
 	})
 
 	Context("with ephemeral disk", func() {
-		It("Should successfully passthrough an emulated PCI device", func() {
+		DescribeTable("with emulated PCI devices", func(deviceIDs []string) {
 			deviceName := "example.org/soundcard"
-			deviceIDs := "8086:2668"
 			kv := util.GetCurrentKv(virtClient)
 
 			By("Adding the emulated sound card to the permitted host devices")
@@ -42,24 +48,22 @@ var _ = Describe("[Serial][sig-compute]HostDevices", func() {
 					MemoryLimit: resource.NewScaledQuantity(2, resource.Giga),
 				},
 			}
-			config.PermittedHostDevices = &v1.PermittedHostDevices{
-				PciHostDevices: []v1.PciHostDevice{
-					{
-						PCIVendorSelector: deviceIDs,
-						ResourceName:      deviceName,
-					},
-				},
+			config.PermittedHostDevices = &v1.PermittedHostDevices{}
+			var hostDevs []v1.HostDevice
+			for i, id := range deviceIDs {
+				config.PermittedHostDevices.PciHostDevices = append(config.PermittedHostDevices.PciHostDevices, v1.PciHostDevice{
+					PCIVendorSelector: id,
+					ResourceName:      deviceName,
+				})
+				hostDevs = append(hostDevs, v1.HostDevice{
+					Name:       fmt.Sprintf("sound%d", i),
+					DeviceName: deviceName,
+				})
 			}
 			tests.UpdateKubeVirtConfigValueAndWait(config)
 
 			By("Creating a Fedora VMI with the sound card as a host device")
 			randomVMI := tests.NewRandomFedoraVMIWithGuestAgent()
-			hostDevs := []v1.HostDevice{
-				{
-					Name:       "sound",
-					DeviceName: deviceName,
-				},
-			}
 			randomVMI.Spec.Domain.Devices.HostDevices = hostDevs
 			vmi, err := virtClient.VirtualMachineInstance(util.NamespaceTestDefault).Create(randomVMI)
 			Expect(err).ToNot(HaveOccurred())
@@ -67,10 +71,19 @@ var _ = Describe("[Serial][sig-compute]HostDevices", func() {
 			Expect(console.LoginToFedora(vmi)).To(Succeed())
 
 			By("Making sure the sound card is present inside the VMI")
-			Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
-				&expect.BSnd{S: "grep -c " + strings.Replace(deviceIDs, ":", "", 1) + " /proc/bus/pci/devices\n"},
-				&expect.BExp{R: console.RetValue("1")},
-			}, 15)).To(Succeed(), "Device not found")
-		})
+			for _, id := range deviceIDs {
+				Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
+					&expect.BSnd{S: "grep -c " + strings.Replace(id, ":", "", 1) + " /proc/bus/pci/devices\n"},
+					&expect.BExp{R: console.RetValue("1")},
+				}, 15)).To(Succeed(), "Device not found")
+			}
+			// Make sure to delete the VMI before ending the test otherwise a resource could still be taken
+			err = virtClient.VirtualMachineInstance(util.NamespaceTestDefault).Delete(vmi.ObjectMeta.Name, &metav1.DeleteOptions{})
+			Expect(err).To(BeNil(), failedDeleteVMI)
+			tests.WaitForVirtualMachineToDisappearWithTimeout(vmi, 180)
+		},
+			Entry("Should successfully passthrough an emulated PCI device", []string{"8086:2668"}),
+			Entry("Should successfully passthrough 2 emulated PCI devices", []string{"8086:2668", "8086:2415"}),
+		)
 	})
 })
