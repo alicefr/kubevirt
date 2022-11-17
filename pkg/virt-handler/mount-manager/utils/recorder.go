09b182858fc1dda475523307ca0b2ab583d17749
Alice Frosi <afrosi@redhat.com>
2022-11-17 17:37:57 +0100

    Refactor recoder

--- pkg/virt-handler/mount-manager/utils/recorder.go
+++ pkg/virt-handler/mount-manager/utils/recorder.go
@@ -3,11 +3,11 @@ package recorder
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sync"
 
+	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/types"
 	v1 "kubevirt.io/api/core/v1"
 
@@ -22,26 +22,114 @@ type MountTargetEntry struct {
 	SocketFile string `json:"socketFile,omitempty"`
 }
 
-type VMIMountTargetEntry struct {
+type VMIMountTargetRecord struct {
 	HotpluggedVolumes []MountTargetEntry `json:"hotpluggedDisks"`
 	ContainerDisks    []MountTargetEntry `json:"containerDisks"`
-}
-
-type VMIMountTargetRecord struct {
-	MountTargetEntries VMIMountTargetEntry `json:"mountTargetEntries"`
-	UsesSafePaths      bool                `json:"usesSafePaths"`
+	UsesSafePaths     bool               `json:"usesSafePaths"`
 }
 
 func (r *VMIMountTargetRecord) GetContainerDisks() []MountTargetEntry {
-	return r.MountTargetEntries.ContainerDisks
+	return r.ContainerDisks
 }
 
 func (r *VMIMountTargetRecord) GetHotpluggedVolumes() []MountTargetEntry {
-	return r.MountTargetEntries.HotpluggedVolumes
+	return r.HotpluggedVolumes
 }
 
 func (record *VMIMountTargetRecord) IsEmpty() bool {
-	return len(record.MountTargetEntries.ContainerDisks) == 0 && len(record.MountTargetEntries.HotpluggedVolumes) == 0
+	return len(record.ContainerDisks) == 0 && len(record.HotpluggedVolumes) == 0
+}
+
+const (
+	containerDiskMountStates     = "container-disk-mount-state"
+	hotpluggedVolumesMountStates = "hotplug-volume-mount-state"
+)
+
+type vmiMountTargetRecordForCache struct {
+	MountTargetEntries []MountTargetEntry `json:"mountTargetEntries"`
+	UsesSafePaths      bool               `json:"usesSafePaths"`
+}
+
+func readRecordFile(recordFile string) ([]MountTargetEntry, bool, error) {
+	record := vmiMountTargetRecordForCache{}
+	// #nosec No risk for path injection. Using static base and cleaned filename
+	bytes, err := os.ReadFile(recordFile)
+	if err != nil {
+		return []MountTargetEntry{}, false, err
+	}
+	err = json.Unmarshal(bytes, &record)
+	if err != nil {
+		return []MountTargetEntry{}, false, err
+	}
+	return record.MountTargetEntries, record.UsesSafePaths, nil
+}
+
+func (m *mounter) readRecordFiles(uid string) (*VMIMountTargetRecord, bool, error) {
+	record := &VMIMountTargetRecord{}
+	var useSafepathsCd, useSafepathsHp bool
+	// Read the container disks entries from the filesystem
+	// if not there, see if record is on disk, this can happen if virt-handler restarts
+	recordFile := filepath.Join(m.mountStateDir, containerDiskMountStates, filepath.Clean(uid))
+	existsCds, err := diskutils.FileExists(recordFile)
+	if err != nil {
+		return nil, false, err
+	}
+	if existsCds {
+		record.ContainerDisks, useSafepathsCd, err = readRecordFile(recordFile)
+		if err != nil {
+			return nil, false, err
+		}
+	}
+
+	// Read hotplugged volumes entries from the filesystem
+	recordFile = filepath.Join(m.mountStateDir, hotpluggedVolumesMountStates, filepath.Clean(uid))
+	if err != nil {
+		return nil, false, err
+	}
+	existsHps, err := diskutils.FileExists(recordFile)
+	if err != nil {
+		return nil, false, err
+	}
+	if existsHps {
+		record.HotpluggedVolumes, useSafepathsHp, err = readRecordFile(recordFile)
+		if err != nil {
+			return nil, false, err
+		}
+	}
+	record.UsesSafePaths = useSafepathsHp && useSafepathsCd
+	return record, existsCds || existsHps, nil
+}
+
+func writeRecordFile(recordFile string, record []MountTargetEntry) error {
+	r := vmiMountTargetRecordForCache{
+		MountTargetEntries: record,
+		// XXX: backward compatibility for old unresolved paths, can be removed in July 2023
+		// After a one-time convert and persist, old records are safe too.
+		UsesSafePaths: true,
+	}
+	bytes, err := json.Marshal(r)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(filepath.Dir(recordFile), 0750)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(recordFile, bytes, 0600)
+}
+
+func (m *mounter) writeRecordFiles(uid string, record *VMIMountTargetRecord) error {
+	errCd := writeRecordFile(filepath.Join(m.mountStateDir, containerDiskMountStates, uid), record.ContainerDisks)
+	errHp := writeRecordFile(filepath.Join(m.mountStateDir, hotpluggedVolumesMountStates, uid), record.HotpluggedVolumes)
+	if errCd != nil || errHp != nil {
+		if errCd != nil {
+			return errors.Wrap(errCd, errHp.Error())
+		}
+		return errHp
+	}
+	return nil
 }
 
 type RecordEntry int
@@ -85,9 +173,9 @@ func (m *mounter) SetAddMountRecordContainerDisk(vmi *v1.VirtualMachineInstance,
 		record = &VMIMountTargetRecord{}
 	}
 	if addPreviousRules {
-		record.MountTargetEntries.ContainerDisks = append(record.MountTargetEntries.ContainerDisks, cdRecord...)
+		record.ContainerDisks = append(record.ContainerDisks, cdRecord...)
 	} else {
-		record.MountTargetEntries.ContainerDisks = cdRecord
+		record.ContainerDisks = cdRecord
 	}
 
 	return m.setMountTargetRecord(vmi, record)
@@ -103,7 +191,7 @@ func (m *mounter) SetMountRecordHotpluggedVolumes(vmi *v1.VirtualMachineInstance
 		// Initialize it
 		record = &VMIMountTargetRecord{}
 	}
-	record.MountTargetEntries.HotpluggedVolumes = hpRecord
+	record.HotpluggedVolumes = hpRecord
 
 	return m.setMountTargetRecord(vmi, record)
 }
@@ -124,9 +212,6 @@ func (m *mounter) GetContainerDisksMountRecord(vmi *v1.VirtualMachineInstance) (
 	if record == nil {
 		return []MountTargetEntry{}, nil
 	}
-	if record == nil {
-		return []ContainerDisksMountTargetEntry{}, nil
-	}
 	return record.GetContainerDisks(), nil
 }
 
@@ -137,9 +222,6 @@ func (m *mounter) GetHotpluggedVolumesMountRecord(vmi *v1.VirtualMachineInstance
 	}
 	if record == nil {
 		return []MountTargetEntry{}, nil
-	}
-	if record == nil {
-		return []HotpluggedDisksMountTargetEntry{}, nil
 	}
 	return record.GetHotpluggedVolumes(), nil
 }
@@ -161,23 +243,23 @@ func (m *mounter) deleteMountTargetRecord(vmi *v1.VirtualMachineInstance, entry 
 			return err
 		}
 		r, ok := m.mountRecords[vmi.UID]
-		for _, target := range record.MountTargetEntries.ContainerDisks {
+		for _, target := range record.ContainerDisks {
 			if entry != CONTAINERDISKS_ENTRY && entry != ALL_ENTRIES {
 				break
 			}
 			os.Remove(target.TargetFile)
 			os.Remove(target.SocketFile)
 			if ok {
-				r.MountTargetEntries.ContainerDisks = []MountTargetEntry{}
+				r.ContainerDisks = []MountTargetEntry{}
 			}
 		}
-		for _, target := range record.MountTargetEntries.HotpluggedVolumes {
+		for _, target := range record.HotpluggedVolumes {
 			if entry != HOTPLUGGEDVOLUMES_ENTRY && entry != ALL_ENTRIES {
 				break
 			}
 			os.Remove(target.TargetFile)
 			if ok {
-				r.MountTargetEntries.HotpluggedVolumes = []MountTargetEntry{}
+				r.HotpluggedVolumes = []MountTargetEntry{}
 			}
 		}
 		if r.IsEmpty() {
@@ -217,47 +299,33 @@ func (m *mounter) getMountTargetRecord(vmi *v1.VirtualMachineInstance) (*VMIMoun
 		return existingRecord, nil
 	}
 
-	// if not there, see if record is on disk, this can happen if virt-handler restarts
-	recordFile := filepath.Join(m.mountStateDir, filepath.Clean(string(vmi.UID)))
-
-	exists, err := diskutils.FileExists(recordFile)
+	record, exists, err := m.readRecordFiles(string(vmi.UID))
 	if err != nil {
 		return nil, err
 	}
-
 	if exists {
-		record := VMIMountTargetRecord{}
-		// #nosec No risk for path injection. Using static base and cleaned filename
-		bytes, err := os.ReadFile(recordFile)
-		if err != nil {
-			return nil, err
-		}
-		err = json.Unmarshal(bytes, &record)
-		if err != nil {
-			return nil, err
-		}
 		// XXX: backward compatibility for old unresolved paths, can be removed in July 2023
 		// After a one-time convert and persist, old records are safe too.
 		if !record.UsesSafePaths {
 			record.UsesSafePaths = true
-			for i, entry := range record.MountTargetEntries.ContainerDisks {
+			for i, entry := range record.ContainerDisks {
 				safePath, err := safepath.JoinAndResolveWithRelativeRoot("/", entry.TargetFile)
 				if err != nil {
 					return nil, fmt.Errorf("failed converting legacy path to safepath: %v", err)
 				}
-				record.MountTargetEntries.ContainerDisks[i].TargetFile = unsafepath.UnsafeAbsolute(safePath.Raw())
+				record.ContainerDisks[i].TargetFile = unsafepath.UnsafeAbsolute(safePath.Raw())
 			}
-			for i, entry := range record.MountTargetEntries.HotpluggedVolumes {
+			for i, entry := range record.HotpluggedVolumes {
 				safePath, err := safepath.JoinAndResolveWithRelativeRoot("/", entry.TargetFile)
 				if err != nil {
 					return nil, fmt.Errorf("failed converting legacy path to safepath: %v", err)
 				}
-				record.MountTargetEntries.HotpluggedVolumes[i].TargetFile = unsafepath.UnsafeAbsolute(safePath.Raw())
+				record.HotpluggedVolumes[i].TargetFile = unsafepath.UnsafeAbsolute(safePath.Raw())
 			}
 		}
 
-		m.mountRecords[vmi.UID] = &record
-		return &record, nil
+		m.mountRecords[vmi.UID] = record
+		return record, nil
 	}
 
 	// not found
@@ -268,30 +336,13 @@ func (m *mounter) setMountTargetRecord(vmi *v1.VirtualMachineInstance, record *V
 	if string(vmi.UID) == "" {
 		return fmt.Errorf("unable to find mounted directories for vmi without uid")
 	}
-
-	// XXX: backward compatibility for old unresolved paths, can be removed in July 2023
-	// After a one-time convert and persist, old records are safe too.
-	record.UsesSafePaths = true
-
-	recordFile := filepath.Join(m.mountStateDir, string(vmi.UID))
-
 	m.mountRecordsLock.Lock()
 	defer m.mountRecordsLock.Unlock()
 
-	bytes, err := json.Marshal(record)
-	if err != nil {
+	if err := m.writeRecordFiles(string(vmi.UID), record); err != nil {
 		return err
 	}
 
-	err = os.MkdirAll(filepath.Dir(recordFile), 0750)
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(recordFile, bytes, 0600)
-	if err != nil {
-		return err
-	}
 	m.mountRecords[vmi.UID] = record
 	return nil
 }
