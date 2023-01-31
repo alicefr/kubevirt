0a39fd145fa4b3ba0bfaf72e0fea06fce0604539
Alice Frosi <afrosi@redhat.com>
2023-01-31 09:26:50 +0100

    storage: create package for the persistent reservation
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

---
+++ pkg/storage/reservation/pr.go
@@ -0,0 +1,48 @@
+package reservation
+
+import (
+	"fmt"
+	"path/filepath"
+
+	v1 "kubevirt.io/api/core/v1"
+)
+
+const (
+	SourceDaemonsPath = "/run/kubevirt/daemons"
+)
+
+const (
+	PrHelperDir    = "pr"
+	PrHelperSocket = "pr-helper.sock"
+	PrVolumeName   = "pr-socket-volume"
+	PrResourceName = "pr-helper"
+)
+
+func GetPrResourceName() string {
+	return PrResourceName
+}
+
+func GetPrHelperSocketDir() string {
+	return fmt.Sprintf(filepath.Join(SourceDaemonsPath, PrHelperDir))
+}
+
+func GetPrHelperSocketPath() string {
+	return fmt.Sprintf(filepath.Join(GetPrHelperSocketDir(), PrHelperSocket))
+}
+
+func GetPrHelperSocket() string {
+	return PrHelperSocket
+}
+
+func HasVMIPersistentReservation(vmi *v1.VirtualMachineInstance) bool {
+	return HasVMISpecPersistentReservation(&vmi.Spec)
+}
+
+func HasVMISpecPersistentReservation(vmiSpec *v1.VirtualMachineInstanceSpec) bool {
+	for _, disk := range vmiSpec.Domain.Devices.Disks {
+		if disk.DiskDevice.LUN != nil && disk.DiskDevice.LUN.Reservation {
+			return true
+		}
+	}
+	return false
+}
